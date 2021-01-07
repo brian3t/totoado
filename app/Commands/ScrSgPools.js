ebbbd5388d9b9cb4a35fb46ebc80b3f449ba8fe7
brian3t <ngxtri@gmail.com>
2021-01-06 23:14:40 -0800

    Scraping done

--- app/Commands/ScrSgPools.js
+++ app/Commands/ScrSgPools.js
@@ -7,6 +7,7 @@ const mm = require('moment')
 const Database = use('Database')
 const TOTO_URL = 'https://www.singaporepools.com.sg/DataFileArchive/Lottery/Output/toto_result_top_draws_en.html' //optional: ?v=2021y1m3d
 const TotoResult = use('App/Models/TotoResult')
+const WinningGroup = use('App/Models/WinningGroup')
 
 class ScrSgPools extends Command {
   static get signature(){
@@ -75,110 +76,34 @@ class ScrSgPools extends Command {
         /** @type {Cheerio} **/
         let $more_winning_trs = await $more_win_groups_th.closest('table').find('tbody > tr')
         let winning_groups = $more_winning_trs.map((i, morewin) => {
-          if (i <= 2) return //skip the first two tr; they are header and empty group 1 winning
-          if (morewin.type === 'tag') return {
-            group_tier: i + 1,
-            amount: morewin,
-            num_of_winning_shares: 3,
+          if (i === 0) return //skip the first tr; header
+          if (morewin.type !== 'tag') return
+          let tds = $(morewin).find('td')
+          let amount = $(tds[1]).text().replace(/\$/g, '').replace(/,/g, '')
+          let num_of_winning_shares = parseInt($(tds[2]).text())
+          if (isNaN(num_of_winning_shares)) num_of_winning_shares = null
+          return {
+            group_tier: i,
+            amount: amount,
+            num_of_winning_shares: num_of_winning_shares,
             toto_result_id: toto_model.id
           }
         })
-
-        //now get sibling div containing list of games
-        let div_list_of_games = $result_date.next('div.responsive-table-wrap')
-        if (typeof div_list_of_games !== 'object' || ! (div_list_of_games[0])) return
-        div_list_of_games = $(div_list_of_games[0]) //my immediate next sibling
-
-        await div_list_of_games.find('tbody > tr').each(async (i, game_tr) => {//for each game row
-          let $game_tr = $(game_tr)
-          let $game_tds = $($game_tr.find('td'))
-          if (! $game_tds || ! $game_tds[0]) return
-
-          let $first_td = $($game_tds[0]) //first td, away team
-          let $a_team_name = $($first_td.find('a.team-name'))
-          if ($a_team_name.length !== 1) return
-          let a_team_name_abbr = $a_team_name.find('abbr').attr('title')
-          let away_team_found = await Team.findBy('name', a_team_name_abbr)
-          if (! (away_team_found instanceof Team)) away_team_found = await Team.findBy('short_name', a_team_name_abbr)
-          if (! (away_team_found instanceof Team)) {
-            console.error(`cant find away team`)
-            return
-          }
-
-          if (! $game_tds[1]) return
-          let $second_td = $($game_tds[1]) //second td, home team
-          $a_team_name = $($second_td.find('a.team-name'))
-          if ($a_team_name.length !== 1) return
-          a_team_name_abbr = $a_team_name.find('abbr').attr('title')
-          let home_team_found = await Team.findBy('name', a_team_name_abbr)
-          if (! (home_team_found instanceof Team)) home_team_found = await Team.findBy('short_name', a_team_name_abbr)
-          if (! (home_team_found instanceof Team)) {
-            console.error(`cant find home team`)
-            return
-          }
-
-          //now that we have hometeam and away team, let's find existing game
-          if (! home_team_found || ! home_team_found.id || ! away_team_found || ! away_team_found.id || ! result_date_db) {
-            console.error(`Missing home / away / gamedate. Hometeamfound: `, home_team_found, ` Ateam found: `, away_team_found, ` game_date_scraped: ${result_date_db}`)
-            return
-          }
-          let exist_or_new_game = await Game.findOrCreate(
-            {
-              teamh_id: home_team_found.id, teama_id: away_team_found.id, game_date: result_date_db
-            },
-            {
-              teamh_id: home_team_found.id, teama_id: away_team_found.id, game_date: result_date_db
-            })
-          if (! (exist_or_new_game instanceof Game)) return
-          if (! $game_tds[2]) return
-          let $third_td = $($game_tds[2]) //third td
-          if ($third_td.attr('data-date')) { //UPCOMING GAME
-            let game_date_time = mm($third_td.attr('data-date').replace('Z', ''), 'YYYY-MM-DDTHH:mm') //2020-11-01T21:05Z
-            if (! game_date_time.isValid()) return
-            exist_or_new_game.game_time = game_date_time.format('HH:mm')
-            await exist_or_new_game.save()
-            let td_location = $($game_tr.find('td.schedule-location')).text()
-            exist_or_new_game.location_text = td_location
-            await exist_or_new_game.save()
-          } else { // PAST GAME
-            let a_result = $third_td.find('a[name="&lpos=nfl:schedule:score"]')
-            if (a_result.length === 1) {
-              a_result = a_result.text() // ATL 25, CAR 17
-              let all_teams_all_points = [] //store all teams' points
-              let points = a_result.split(',')
-              if (points.length === 2) {
-                let point_1 = points[0].split(' ')
-                if (point_1.length === 2) {
-                  all_teams_all_points.push(point_1)
-                }
-                let point_2 = points[0].split(' ')
-                if (point_2.length === 2) {
-                  all_teams_all_points.push(point_2)
-                }
-              }
-              all_teams_all_points.forEach((team_point) => {
-                let team_abbr = team_point[0], point = team_point[1] //ATL 25
-                if (home_team_found.abbr === team_abbr) {
-                  exist_or_new_game.hpoint = point
-                  exist_or_new_game.save()
-                }
-                if (away_team_found.abbr === team_abbr) {
-                  exist_or_new_game.apoint = point
-                  exist_or_new_game.save()
-                }
-              })
-            }
-            let a = 1
-
-          }
+        winning_groups.toArray().forEach((winning_group) => {
+          let wg_create_res = WinningGroup.findOrCreate(winning_group, winning_group)
+          if (wg_create_res instanceof WinningGroup) console.log(`wingroup created`)
         })
-      })
+
+
+      })//end group of LI
+
+
       console.log(`After 5 secs we close db, so that process exit; no matter what is running. Time's up!`)
       setTimeout(() => {
         Database.close()
       }, 5000)
     } catch (e) {
-      console.error(`error 61: `, e)
+      console.error(`error 106: `, e)
     }
   }
 }
